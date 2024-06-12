db51b785100796e5c8f1da330ec18f52243a48bc
felomondi <fomondi@vassar.edu>
2024-06-12 00:20:44 -0400

    add pins

---
+++ backend/routes/pins.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const Pin = require("../Models/Pin");
+
+//create a pin
+
+router.post("/", async (req, res) => {
+
+    const newPin = new Pin(req.body);
+
+    try {
+        const savedPin = await newPin.save();
+        res.status(200).json(savedPin);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//get all pins
+
+module.exports = router;
+
+router.get("/", async (req, res) => {});
